6bf32f841ad659a851c8075b4b05931e8adc964f
Yury Kozyrev <y.kozyrev@travelaudience.com>
2021-03-12 11:03:37 +0100

    rewrite logic

--- src/index.ts
+++ src/index.ts
@@ -14,67 +14,61 @@ interface Config {
   pathWhitelist?: Set<string>
   useCustomAuth?: (req: Request) => Promise<admin.auth.UserRecord | null>
 }
+const getIDToken = (req: Request, enableLogs: boolean) => {
+  if (req.headers.authorization?.startsWith('Bearer ')) {
+    if (enableLogs) {
+      console.log('Found "Authorization" header');
+    }
+    // Read the ID Token from the Authorization header.
+    return req.headers.authorization.split('Bearer ')[1];
+  } else if (req.cookies?.__session) {
+    if (enableLogs) {
+      console.log('Found "__session" cookie');
+    }
+    // Read the ID Token from cookie.
+    return req.cookies.__session;
+  }
+}
 export const validateFirebaseIdToken = (config: Config = {}) => {
-
   return async (req: Request & WithUser, res: Response, next: () => void) => {
     if (config.pathWhitelist?.size > 0 && config.pathWhitelist.has(req.path)) {
       return next();
     }
-    console.log('Check if request is authorized with Firebase ID token', req.headers, req.cookies);
-
-    if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
-      !(req.cookies && req.cookies.__session)) {
-      if (config.enableLogs) {
-        console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
-          'Make sure you authorize your request by providing the following HTTP header:',
-          'Authorization: Bearer <Firebase ID Token>',
-          'or by passing a "__session" cookie.');
-      }
-      res.status(403).send('Unauthorized');
-      return;
+    if (config.enableLogs) {
+      console.log('Check if request is authorized with Firebase ID token', req.headers, req.cookies);
     }
 
-    let idToken;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-      if (config.enableLogs) {
-        console.log('Found "Authorization" header');
-      }
-      // Read the ID Token from the Authorization header.
-      idToken = req.headers.authorization.split('Bearer ')[1];
-    } else if (req.cookies) {
-      if (config.enableLogs) {
-        console.log('Found "__session" cookie');
-      }
-      // Read the ID Token from cookie.
-      idToken = req.cookies.__session;
-    } else {
-      // No cookie
-      res.status(403).send('Unauthorized');
-      return;
-    }
-
+    const userData: WithUser = {}
     try {
+      const idToken = getIDToken(req, config.enableLogs)
       const decodedIdToken = await admin.auth().verifyIdToken(idToken);
       if (config.enableLogs) {
         console.log('ID Token correctly decoded', decodedIdToken);
       }
       const user = await admin.auth().getUser(decodedIdToken.uid)
-      req.decodedIdToken = decodedIdToken;
-      req.user = user;
-      return next();
-    } catch (error) {
-      if (config.useCustomAuth) {
-        const user = await config.useCustomAuth(req);
-        if (user) {
-          req.user = user;
-          return next();
-        }
-      }
+      userData.decodedIdToken = decodedIdToken
+      userData.user = user
+    } catch (e) {
       if (config.enableLogs) {
-        console.error('Error while verifying Firebase ID token:', error);
+        console.log(`Error while verifying Firebase ID token: `, e);
       }
-      res.status(403).send('Unauthorized');
-      return;
     }
+    if (config.useCustomAuth) {
+      userData.user = await config.useCustomAuth(req);
+      if (config.enableLogs) {
+        console.log('Authorising with customAuth: ', userData.user ? "Success" : "Fail");
+      }
+    }
+    if (userData.user) {
+      Object.assign(req, userData)
+      return next();
+    }
+    if (config.enableLogs) {
+      console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>',
+        ', by passing a "__session" cookie or using customAuth');
+    }
+    res.status(403).send('Unauthorized');
   };
 }
